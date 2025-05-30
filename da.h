75e30ba84f1bd57b40afdf8cba47f69aaa45ad48
PaulUlanovskij <ulanovskijpasa@gmail.com>
2025-05-30 13:43:45 +0300

    parametrized da initial capacity user_defined, added way of adding multiple elemnts to da, and other minor changes

--- da.h
+++ da.h
@@ -1,10 +1,17 @@
+#ifndef DA_INIT_CAPACITY
+#define DA_INIT_CAPACITY 16
+#endif // DA_INIT_CAPACITY
+
 #define da_reserve(xs, size)                                                   \
   do {                                                                         \
-    if ((xs)->capacity < size) {                                               \
-      (xs)->capacity = size;                                                   \
+    while ((xs)->capacity < size) {                                            \
+      if ((xs)->capacity == 0)                                                 \
+        (xs)->capacity = DA_INIT_CAPACITY;                                     \
+      else                                                                     \
+        (xs)->capacity *= 2;                                                   \
     }                                                                          \
-    (xs)->items =                                                              \
-        (typeof((xs)->items)) realloc((xs)->items, (xs)->capacity * sizeof(*(xs)->items));    \
+    (xs)->items = (typeof((xs)->items))realloc(                                \
+        (xs)->items, (xs)->capacity * sizeof(*(xs)->items));                   \
   } while (0)
 
 #define da_foreach(xs, name)                                                   \
@@ -18,11 +25,11 @@
     (xs)->length = 0;                                                          \
   } while (0)
 
-#define da_copy_terminate(dest, src)                                           \
+#define da_append_null(xs)                                                     \
   do {                                                                         \
-    (dest) = (typeof((src)->items))calloc(((src)->length + 1),                 \
-                                          sizeof(*(src)->items));              \
-    memcpy((dest), (src)->items, sizeof(*(src)->items) * ((src)->length));     \
+    da_reserve((xs), (xs)->length + 1);                                        \
+    memset((xs)->items + (xs)->length, 0, sizeof(*(xs)->items));               \
+    (xs)->length++;                                                            \
   } while (0)
 
 #define da_copy(dest, src)                                                     \
@@ -34,15 +41,15 @@
 
 #define da_append(xs, x)                                                       \
   do {                                                                         \
-    if ((xs)->length == (xs)->capacity) {                                      \
-      if ((xs)->capacity == 0)                                                 \
-        (xs)->capacity = 16;                                                   \
-      else                                                                     \
-        (xs)->capacity *= 2;                                                   \
-    }                                                                          \
-    (xs)->items = (typeof((xs)->items))realloc(                                \
-        (xs)->items, (xs)->capacity * sizeof(*(xs)->items));                   \
+    da_reserve((xs), (xs)->length + 1);                                        \
     (xs)->items[(xs)->length++] = (x);                                         \
   } while (0)
 
-
+#define da_append_many(xs, name, as, n)                                        \
+  do {                                                                         \
+    da_reserve((xs), (xs)->length + n);                                        \
+    for (typeof((as)) name = (as); name < (as) + n; name++) {                  \
+      (xs)->items[(xs)->length] = name;                                        \
+      (xs)->length++;                                                          \
+    }                                                                          \
+  } while (0)
